e9cd7030fdb8f4be82d3214d763f320db313bbdb
unknown <ricknogueira1231@gmail.com>
2024-10-19 18:34:29 -0300

    Ajustando o nome do projeto

--- src/main/java/br/ufpa/traffic_api/AulaApiApplication.java
+++ src/main/java/br/ufpa/traffic_api/AulaApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TrafficApiApplication {
+public class AulaApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TrafficApiApplication.class, args);
+		SpringApplication.run(AulaApiApplication.class, args);
 	}
 
 }
